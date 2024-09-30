bd20e462943420eace5edf399e7d3fee20762277
Tolga Tarhan <ttarhan@users.noreply.github.com>
2024-09-30 10:34:53 -0700

    Use c1 card

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-amixer -d -c 3 cset "numid=9,iface=MIXER,name='Auto Gain Control'" 0
-amixer -d -c 3 cset "numid=8,iface=MIXER,name='Mic Capture Volume'" $CAPTURE_GAIN
+#!/bin/bash
+
+set -o xtrace
+
+amixer -d -c 1 cset "numid=9,iface=MIXER,name='Auto Gain Control'" 0
+amixer -d -c 1 cset "numid=8,iface=MIXER,name='Mic Capture Volume'" $CAPTURE_GAIN
 
 pdm run python main.py
