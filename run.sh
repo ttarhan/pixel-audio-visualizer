5cbc86b3a248d44c21e7918e6facf23419af3252
Tolga Tarhan <ttarhan@users.noreply.github.com>
2025-04-14 22:48:56 -0700

    Update for pi5
    feat: update for pi5

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 set -o xtrace
 
-amixer -d -c 1 cset "numid=9,iface=MIXER,name='Auto Gain Control'" 0
-amixer -d -c 1 cset "numid=8,iface=MIXER,name='Mic Capture Volume'" $CAPTURE_GAIN
+amixer -d -c 0 cset "numid=9,iface=MIXER,name='Auto Gain Control'" 0
+amixer -d -c 0 cset "numid=8,iface=MIXER,name='Mic Capture Volume'" $CAPTURE_GAIN
 
 # nice -n -5 .venv/bin/visualizercli
  chrt -f 1 .venv/bin/visualizercli
