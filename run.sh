28ff4f12332e2da8b671c941c0e931a59773da45
Tolga Tarhan <ttarhan@users.noreply.github.com>
2025-09-17 09:31:30 -0700

    feat: Add external config to chart

--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-set -o xtrace
+set -xeuo pipefail
+
+# Detect if running in k8s
+if [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ] || [ "$KUBERNETES_SERVICE_HOST" != "" ]; then
+	# Only run this block if in k8s
+	if [ ! -f /data/config.py ]; then
+		cp /app/visualizer/config.py /data/config.py
+	fi
+	cp /data/config.py /app/visualizer/config.py
+
+	# Allow environment variables to be set in /data/env
+	if [ ! -f /data/env ]; then
+		touch /data/env
+	fi
+
+    set -a
+    . /data/env
+    set +a
+fi
 
 amixer -d -c 0 cset "numid=9,iface=MIXER,name='Auto Gain Control'" 0
 amixer -d -c 0 cset "numid=8,iface=MIXER,name='Mic Capture Volume'" $CAPTURE_GAIN
